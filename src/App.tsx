0435452f6c4591f0a780d2f8d279a6056522d1bb
Marcelo Rangel Barros <marcelorangelb2006@gmail.com>
2025-07-13 17:38:27 -0300

    feat: movimentações salvas no Supabase e saldo atualizado automaticamente

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
+import EntradaSaida from './pages/EntradaSaida';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/entrada-saida" element={<EntradaSaida />} />
       </Routes>
     </Router>
   );
